3e5f0800c32a87f7f4756acb0d531fe89b8eb0e8
thanigai arasu <thanigaiarasu25@gmail.com>
2026-06-01 21:23:11 +0000

    Initial commit

---
+++ sapui5/webapp/controller/View1.controller.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("sap.btp.sapui5.controller.View1", {
+        onInit() {
+        }
+//             onBeforeRendering: function () {
+//                window.onmessage = "A random log message";
+//                Log.info(window.onmessage);
+//            }
+//                        onAfterRendering: function () {
+//                debugger;
+//            }
+    });
+});
